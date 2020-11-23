188b00dae1411ee4c61bae215cb121850e1c7e0f
Rohit <muralirohit@gmail.com>
2020-11-24 02:23:13 +0530

    some more results

--- ML/print_results.py
+++ ML/print_results.py
@@ -4,10 +4,19 @@ import numpy as np
 import pickle
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+pd.set_option('display.max_columns', None)  # or 1000
+pd.set_option('display.max_rows', None)  # or 1000
+pd.set_option('display.max_colwidth', None)  # or 199
+
 ep=["Frustration","Anxiety"]
 # ep=["Boredom"]
 eye_window='full'
 log_window='full'
+smote = False
+if smote:
+    results='/results_smote/'
+else:
+    results='/results/'
 
 if len(ep)==1:
     type='/single'
@@ -25,7 +34,7 @@ for threshold in ['3','4']:
         accs=[]
         accuracies['Model']=['Baseline','SVM','LR','RF','NN']
         for model in ['SVM','LR','RF','NN']:
-            with open(dir_path+type+'/results/'+eps+'/'+folder+'/'+model+result_suffix+'_'+eps+'_'+data+'.pickle', 'rb') as handle:
+            with open(dir_path+type+results+eps+'/'+folder+'/'+model+result_suffix+'_'+eps+'_'+data+'.pickle', 'rb') as handle:
                 res=pickle.load(handle)
             baseline_accuracy=res['baseline_accuracy']
             accs.append(res['mean_accuracy'])
@@ -33,7 +42,11 @@ for threshold in ['3','4']:
 print(accuracies.T)
 accuracies.T.to_excel(dir_path+'/temp_excel.ods')
 
-def class_accuracy(eye_window,log_window,ep,data,threshold,model):
+def class_accuracy(smote,eye_window,log_window,ep,data,threshold,models):
+    if smote:
+        results='/results_smote/'
+    else:
+        results='/results/'
     if len(ep)==1:
         type='/single'
         eps=ep[0]
@@ -44,21 +57,28 @@ def class_accuracy(eye_window,log_window,ep,data,threshold,model):
         classes = ['None',ep[0],ep[1],ep[0]+' and '+ ep[1]]
     folder='/'+eye_window+'_'+log_window
     result_suffix='_'+eye_window+'_'+log_window+'_'+threshold
-    with open(dir_path+type+'/results/'+eps+'/'+folder+'/'+model+result_suffix+'_'+eps+'_'+data+'.pickle', 'rb') as handle:
-        res=pickle.load(handle)
-    conf=res['mean_confusion_matrix']
-    accs=[]
     class_acc=pd.DataFrame()
-    for i in range(len(classes)):
-        accs.append(conf[i][i]/conf[i].sum()*100)
     class_acc['Class']=classes
-    class_acc['Accuracy']=accs
+    for model in models:
+        with open(dir_path+type+results+eps+'/'+folder+'/'+model+result_suffix+'_'+eps+'_'+data+'.pickle', 'rb') as handle:
+            res=pickle.load(handle)
+        conf=res['mean_confusion_matrix']
+        accs=[]
+        base=[]
+        for i in range(len(classes)):
+            accs.append(conf[i][i]/conf[i].sum()*100)
+            base.append(conf[i].sum()/conf.sum()*100)
+        class_acc[model + ' Accuracy']=accs
+    class_acc['Weightage']=base
     class_acc=class_acc.set_index(['Class'])
     return(class_acc)
 
-ep=["Frustration","Anxiety"]
-# ep=["Frustration","Boredom"]
+ep=["Frustration","Boredom"]
 # ep=["Curiosity"]
+# ep=["Frustration","Anxiety"]
 # ep=["Boredom"]
-print(class_accuracy('full','full',ep,'log','4','RF'))
+smote = False
+# models=['RF','SVM','LR','NN']
+models=['RF']
+print(class_accuracy(smote,'15s','full',ep,'log','4',models))
 print('\n')
--- ML/single/load_data.py
+++ ML/single/load_data.py
@@ -6,16 +6,17 @@ import numpy as np
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-datafiles_thres=['/../../Combined_Data/data_full_full_3.pkl','/../../Combined_Data/data_15s_full_3.pkl','/../../Combined_Data/data_full_full_4.pkl','/../../Combined_Data/data_15s_full_4.pkl', '/../../Combined_Data/data_full_half_3.pkl','/../../Combined_Data/data_15s_half_3.pkl','/../../Combined_Data/data_full_half_4.pkl','/../../Combined_Data/data_15s_half_4.pkl']
-result_suffixes=['_full_full_3','_15s_full_3','_full_full_4','_15s_full_4', '_full_half_3','_15s_half_3','_full_half_4','_15s_half_4']
-folders=['/full_full','/15s_full','/full_full','/15s_full', '/full_half','/15s_half','/full_half','/15s_half']
-num=2#index for choosing data
+
+datafiles_thres=['/../../Combined_Data/data_full_full_3.pkl','/../../Combined_Data/data_full_full_4.pkl','/../../Combined_Data/data_15s_full_3.pkl','/../../Combined_Data/data_15s_full_4.pkl', '/../../Combined_Data/data_full_half_3.pkl','/../../Combined_Data/data_full_half_4.pkl','/../../Combined_Data/data_15s_half_3.pkl','/../../Combined_Data/data_15s_half_4.pkl']
+result_suffixes=['_full_full_3','_full_full_4','_15s_full_3','_15s_full_4', '_full_half_3','_full_half_4','_15s_half_3','_15s_half_4']
+folders=['/full_full','/full_ful l','/15s_full','/15s_full', '/full_half','/full_half','/15s_half','/15s_half']
+num=7#index for choosing data
 
 data=pd.read_pickle(dir_path+datafiles_thres[num])
 result_suffix=result_suffixes[num]
 folder=folders[num]
 
-ep=["Boredom"]
+ep=["Curiosity"]
 
 data=data.sort_values(by=['key'])
 np.set_printoptions(threshold=sys.maxsize)
